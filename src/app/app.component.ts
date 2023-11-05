54fbc9993379e19cd25a09223bab4910a327ace5
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-11-05 15:12:39 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./app.component.scss']
+  styles: [`
+    p {
+      color: red;
+      font-size: 2rem;
+      line-height: 1rem
+    }
+    h3 {
+      color: blue;
+      line-height: 0
+    }
+  `]
 })
 export class AppComponent {
   title = 'my-app';
